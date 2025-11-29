99cae7fd74b6a5678ae62230ce0246de0955cd57
pedlopruz <plopezr2011@gmail.com>
2025-11-29 11:27:29 +0100

    Update Backend

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Permitir que el frontend pueda hacer peticiones al backend
+  app.enableCors({
+    origin: 'http://localhost:3000', // URL de tu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
